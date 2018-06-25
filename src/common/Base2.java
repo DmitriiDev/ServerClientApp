6beec79af9aaecc80cf625edb838b784e4bae552
dmitrii.vokuev <dmitrii.vokuev@smaato.com>
2018-06-25 20:39:54 +0200

    save history

--- src/common/Base2.java
+++
@@ -1,8 +0,0 @@
-package common;
-
-public class Base2 implements AuthService {
-    @Override
-    public String getNickByCredentials(String login, String password) {
-        return null;
-    }
-}
